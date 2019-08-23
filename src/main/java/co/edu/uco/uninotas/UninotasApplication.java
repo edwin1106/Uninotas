97e167495c5fe0a4d60657d16f96e123408f3259
edwinosoriogaviria <edwinosoriogaviria@hotmail.com>
2019-08-23 10:29:28 -0500

    Initial commit: Basic infrastructure

---
+++ src/main/java/co/edu/uco/uninotas/UninotasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.uninotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UninotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UninotasApplication.class, args);
+	}
+
+}
